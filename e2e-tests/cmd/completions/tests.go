a2680d858840f6101cfecd7120252163af87e86c
Brice Schaffner <brice.schaffner@swisstopo.ch>
2025-05-06 07:08:55 +0200

    Fix e2e-tests --test tab completion
    Depending on the files on your local machine the search for the infra-e2e-tests
    did not work. So now instead it creates a clone on ~/.e2e-tests folder.

--- e2e-tests/cmd/completions/tests.go
+++ e2e-tests/cmd/completions/tests.go
@@ -1,27 +1,30 @@
 package completions
 
 import (
+	"errors"
 	"fmt"
 	"os"
-	"os/user"
 	"path/filepath"
 	"regexp"
-	"slices"
 	"strings"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 	"github.com/spf13/cobra"
 )
 
 //-----------------------------------------------------------------------------
 
 func CompleteTests(_ *cobra.Command, _ []string, _ string) ([]cobra.Completion, cobra.ShellCompDirective) {
-	repoPath, err := findGitRepo()
+	repoPath, err := getE2ERepo()
 	if err != nil {
+		fmt.Printf("Error finding git repo: %v\n", err)
 		return nil, cobra.ShellCompDirectiveNoFileComp
 	}
 
 	testNames, err := findTests(repoPath)
 	if err != nil {
+		fmt.Printf("Error finding tests: %v\n", err)
 		return nil, cobra.ShellCompDirectiveNoFileComp
 	}
 
@@ -67,58 +70,62 @@ func findTests(repoPath string) ([]string, error) {
 
 //-----------------------------------------------------------------------------
 
-// Find the Git repository for the E2E tests on the local home folder
-func findGitRepo() (string, error) {
-	usr, err := user.Current()
+func getE2ERepo() (string, error) {
+	var repo *git.Repository
+	var err error
+	repoPath := os.Getenv("HOME") + "/.e2e-tests"
+
+	auth, err := ssh.NewSSHAgentAuth("git")
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to create SSH agent auth: %w", err)
 	}
 
-	homeDir := usr.HomeDir
-	gitRepo := "infra-e2e-tests"
-	var gitRepoPath string
-
-	// Walk through the home directory to find a matching .git/config file
-	err = filepath.WalkDir(homeDir, func(path string, entry os.DirEntry, e error) error {
-		if e != nil {
-			return e
+	_, err = os.Stat(repoPath)
+	switch {
+	case os.IsNotExist(err):
+		// folder does not exist, clone the repo
+		repo, err = git.PlainClone(repoPath, false, &git.CloneOptions{
+			URL:           "git@github.com:geoadmin/infra-e2e-tests.git",
+			Auth:          auth,
+			ReferenceName: "refs/heads/master",
+			SingleBranch:  true,
+			Depth:         1,
+			Progress:      nil,
+		})
+		if err != nil {
+			return "", fmt.Errorf("failed to clone repo: %w", err)
 		}
 
-		// skip dot directory (hidden)
-		if strings.HasPrefix(entry.Name(), ".") && entry.IsDir() {
-			return filepath.SkipDir
-		}
+	case err != nil:
+		return "", fmt.Errorf("failed to stat %s: %w", repo, err)
 
-		// skip well known directory
-		if entry.IsDir() && slices.Contains([]string{
-			"node_modules",
-			"Downloads",
-			"Pictures",
-			"Music",
-			"snap",
-			"Videos",
-			"Templates",
-			"aws",
-			"go",
-		}, entry.Name()) {
-			return filepath.SkipDir
+	default:
+		// open the existing repo
+		repo, err = git.PlainOpen(repoPath)
+		if err != nil {
+			return "", fmt.Errorf("failed to open existing repo: %w", err)
 		}
+	}
 
-		if entry.Name() == "infra-e2e-tests" {
-			gitRepoPath = path
-			return filepath.SkipAll
-		}
+	// Get the worktree
+	workTree, err := repo.Worktree()
+	if err != nil {
+		return "", fmt.Errorf("failed to get worktree: %w", err)
+	}
 
-		return nil // continue walking
+	// Update the repo
+	err = workTree.Pull(&git.PullOptions{
+		Auth:         auth,
+		Progress:     nil,
+		SingleBranch: true,
+		Depth:        1,
+		Force:        true,
 	})
-	if err != nil {
-		return "", fmt.Errorf("error while trying to find git repo: %w", err)
+	if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+		return "", fmt.Errorf("failed to pull repo: %w", err)
 	}
 
-	if gitRepoPath == "" {
-		return "", fmt.Errorf("git repo %s not found", gitRepo)
-	}
-	return gitRepoPath, nil
+	return repoPath, nil
 }
 
 //-----------------------------------------------------------------------------
