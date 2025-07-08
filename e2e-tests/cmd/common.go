04fea5a01902a1e990b95d85409c8f2aa1fc4f0f
Adrien Kunysz <adrien.kunysz@swisstopo.ch>
2025-07-08 15:56:13 +0200

    PB-1824: don't segfault when test summary is empty.
    Before:
    ```
    $ ./e2e-tests get --test-id e2e-tests-dev-pr:ab737d01-108d-4454-bc56-b2391fe65548
    E2E Tests run found and completed at 2025-07-08 13:25:17.689 +0000 UTC
    E2E tests failed !
    panic: runtime error: invalid memory address or nil pointer dereference
    [signal SIGSEGV: segmentation violation code=0x1 addr=0x10 pc=0x8a62bb]
    
    goroutine 1 [running]:
    http://github.com/geoadmin/tool-golang-bgdi/e2e-tests/cmd.printTestReport ({0xb1e818, 0xc00017adc0}, 0xc000184a80, {0xc0001ee1c0, 0x70}, {0xc000266140, 0x35}, 0x0)
            /home/ltkya/src/tool-golang-bgdi/e2e-tests/cmd/common.go:188 +0x11b
    http://github.com/geoadmin/tool-golang-bgdi/e2e-tests/cmd.printTestResult ({0xb1e818, 0xc00017adc0}, 0xc000184a80, 0xc0004040c0, 0x0)
            /home/ltkya/src/tool-golang-bgdi/e2e-tests/cmd/common.go:162 +0x1ac
    http://github.com/geoadmin/tool-golang-bgdi/e2e-tests/cmd.init.func1 (0xedff00, {0xa4090e?, 0x4?, 0xa40896?})
            /home/ltkya/src/tool-golang-bgdi/e2e-tests/cmd/get.go:76 +0x38c
    http://github.com/spf13/cobra.(*Command ).execute(0xedff00, {0xc00017e860, 0x2, 0x2})
            /home/ltkya/go/pkg/mod/github.com/spf13/cobra@v1.9.1/command.go:1015 +0xaaa
    http://github.com/spf13/cobra.(*Command ).ExecuteC(0xee01c0)
            /home/ltkya/go/pkg/mod/github.com/spf13/cobra@v1.9.1/command.go:1148 +0x46f
    http://github.com/spf13/cobra.(*Command ).Execute(...)
            /home/ltkya/go/pkg/mod/github.com/spf13/cobra@v1.9.1/command.go:1071
    http://github.com/geoadmin/tool-golang-bgdi/e2e-tests/cmd.Execute()
            /home/ltkya/src/tool-golang-bgdi/e2e-tests/cmd/root.go:44 +0x1e
    main.main()
            /home/ltkya/src/tool-golang-bgdi/e2e-tests/main.go:6 +0xf
    ```
    
    After:
    ```
    $ ./e2e-tests get --test-id e2e-tests-dev-pr:ab737d01-108d-4454-bc56-b2391fe65548
    E2E Tests run found and completed at 2025-07-08 13:25:17.689 +0000 UTC
    E2E tests failed !
    Test summary unexpectedly nil for reportARN=arn:aws:codebuild:eu-central-1:974517877189:report/e2e-tests-dev-pr-reports:ab737d01-108d-4454-bc56-b2391fe65548: {0xc0002a2770 <nil> 2025-07-08 13:25:10.103 +0000 UTC 0xc0002a2790 2025-08-07 13:25:10 +0000 UTC 0xc0003fa580 0xc0002a27a0 0xc0002a2780 INCOMPLETE <nil> <nil> TEST {}}
    ```

--- e2e-tests/cmd/common.go
+++ e2e-tests/cmd/common.go
@@ -185,6 +185,12 @@ func printTestReport(
 	if e != nil {
 		return fmt.Errorf("failed to describe test case for reportARN=%s: %w", reportArn, e)
 	}
+	if len(r.Reports) != 1 {
+		return fmt.Errorf("Unexpected number of reports for reportARN=%s: %d", reportArn, len(r.Reports))
+	}
+	if r.Reports[0].TestSummary == nil {
+		return fmt.Errorf("Test summary unexpectedly nil for reportARN=%s: %v", reportArn, r.Reports[0])
+	}
 	nbTests := int(*r.Reports[0].TestSummary.Total)
 
 	// First get the errors
